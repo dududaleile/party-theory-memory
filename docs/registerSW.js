460c455560d6fadb81cf7fabea1ef6838f43ad00
Student <student@physics.com>
2026-06-09 21:17:53 +0800

    Fix asset base path to /party-theory-memory/

--- docs/registerSW.js
+++ docs/registerSW.js
@@ -1,1 +1,1 @@
-if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/sw.js', { scope: '/' })})}
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/party-theory-memory/sw.js', { scope: '/party-theory-memory/' })})}
